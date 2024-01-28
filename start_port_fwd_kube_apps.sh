1705fa9774ba472517dd609eb7871b89e5c91275
dan1dan12345678 <47665212+dan1dan12345678@users.noreply.github.com>
2024-01-28 16:03:50 +0100

    Update start_port_fwd_kube_apps.sh

--- start_port_fwd_kube_apps.sh
+++ start_port_fwd_kube_apps.sh
@@ -5,6 +5,7 @@ commands=(
   "kubectl --namespace default port-forward grafana-64884db568-77c7s 3000 --address 0.0.0.0"
   "kubectl --namespace default port-forward prometheus-server-6b68fbd54b-96zn4 9090 --address 0.0.0.0"
   "kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0"
+  "kubectl port-forward traefik-f4564c4f4-7984f -n kube-system 9000:9000 --address 139.59.144.130"
 )
 
 # Loop through and execute each command in the background
